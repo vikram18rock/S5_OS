20a14a8d6ad743919ec459860d76a28d51db75a1
vikram18rock <v16ash@gmail.com>
2023-11-11 18:34:04 +0530

    Added timer and also refactored code

--- cdd_module.c
+++ cdd_module.c
@@ -12,6 +12,8 @@
 #include<linux/uaccess.h>
 #include<linux/wait.h>
 #include<linux/kthread.h>
+#include<linux/timer.h>
+#include<linux/jiffies.h>
 
 // cdd - Character Device Driver
 
@@ -24,6 +26,7 @@ module_param_array(kernel_version, int, NULL, S_IRUSR | S_IWUSR);
 // variables
 #define default_size 1024
 static int used_len;
+#define TIMEOUT 1000        // milli seconds
 
 // device Number
 dev_t dev_no;
@@ -36,7 +39,7 @@ static struct device* cdd_device;
 // cdd's kernel space buffer pointer
 uint8_t* cdd_kernel_buffer;
 // wait thread
-static struct task_struct *wait_thread;
+static struct task_struct* wait_thread;
 // wait queue head and usageflag
 wait_queue_head_t wait_queue_cdd;
 static int wait_queue_flag = 0;
@@ -56,6 +59,12 @@ static int cdd_release(struct inode* inode, struct file* file);
 static ssize_t cdd_read(struct file* filp, char __user* buf, size_t len, loff_t* off);
 static ssize_t cdd_write(struct file* filp, const char* buf, size_t len, loff_t* off);
 
+//Timer Callback function. This will be called when timer expires
+void timer_callback(struct timer_list* data)
+{
+    pr_info("Timer of %d secs Expired\n", time);
+    timer_expired = 1;
+}
 
 // file operations for the cdev struct
 struct file_operations f_ops =
@@ -70,23 +79,23 @@ struct file_operations f_ops =
 /*
 ** function for the kernle thread
 */
-static int wait_function(void *unused)
+static int wait_function(void* unused)
 {
     // purpose is to print no. of reads and capture event of exit 
-    while(1) {
-            pr_info("Waiting For Event...\n");
-            wait_event_interruptible(wait_queue_cdd, wait_queue_flag != 0 );
-            if(wait_queue_flag == 2 && read_count) {
-                pr_info("Event Came From Write after Read Function\n");
+    while (1) {
+        pr_info("Waiting For Event...\n");
+        wait_event_interruptible(wait_queue_cdd, wait_queue_flag != 0);
+        if (wait_queue_flag == 2 && read_count) {
+            pr_info("Event Came From Write after Read Function\n");
 
-                // indicating desired order of events
-                wait_queue_flag = 3;
+            // indicating desired order of events
+            wait_queue_flag = 3;
 
-                memcpy(user_name, cdd_kernel_buffer, used_len);
-                return 0;
-            }
-            pr_info("Event Came From Read Function - %d\n", ++read_count);
-            wait_queue_flag = 0;
+            memcpy(user_name, cdd_kernel_buffer, used_len);
+            return 0;
+        }
+        pr_info("Event Came From Read Function - %d\n", ++read_count);
+        wait_queue_flag = 0;
     }
     do_exit(0);
     return 0;
@@ -218,6 +227,12 @@ static int __init cdd_init(void)
     }
     strcpy(cdd_kernel_buffer, "Default\n");
 
+    /* setup your timer to call timer_callback */
+    timer_setup(&cdd_timer, timer_callback, 0);
+
+    mod_timer(&cdd_timer, jiffies + msecs_to_jiffies(time * TIMEOUT));
+    pr_info("Timer Started\n");
+
     // initializing a waitqueue
     init_waitqueue_head(&wait_queue_cdd);
 
@@ -255,7 +270,10 @@ r_class:
 */
 static void __exit cdd_exit(void)
 {
-    if (wait_queue_flag == 3) {
+    // remove timer while unloading the module
+    del_timer(&cdd_timer);
+
+    if (wait_queue_flag == 3 && !timer_expired) {
         pr_info("Successfully completed the actions within time\nUser Name is %s", user_name);
     }
     else
